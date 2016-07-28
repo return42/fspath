cb5b098354c6268edd71ea831aa210c36a22c95f
Markus Heiser <markus.heiser@darmarIT.de>
2016-07-28 19:20:07 +0200

    docs: normalized project's documentation
    Signed-off-by: Markus Heiser <markus.heiser@darmarIT.de>

--- fspath/__init__.py
+++ fspath/__init__.py
@@ -7,9 +7,9 @@ time ``os.path.join...`` then you are right here.
 
 :copyright:  Copyright (C) 2016 Markus Heiser
 :e-mail:     *markus.heiser*\ *@*\ *darmarIT.de*
-:license:    GPL Version 2, June 1991 see linux/COPYING for details.
+:license:    GPL Version 2, June 1991 see Linux/COPYING for details.
 :docs:       http://return42.github.io/fspath
-:reposetory: `github return42/fspath <https://github.com/return42/fspath>`_
+:repository: `github return42/fspath <https://github.com/return42/fspath>`_
 
 """
 
