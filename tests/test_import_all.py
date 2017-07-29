3dbe3256493e7373600025d0d9de2a4363993f61
Markus Heiser <markus.heiser@darmarit.de>
2017-07-29 08:19:43 +0200

    add test: import all modules from there origin
    Signed-off-by: Markus Heiser <markus.heiser@darmarit.de>

---
+++ tests/test_import_all.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8; mode: python -*-
+"""test OS_ENV"""
+
+from fspath.cli import *
+from fspath.fspath import *
+from fspath.helper import *
+from fspath.main import *
+from fspath.os_env import *
+from fspath.progressbar import *
+from fspath.win import *
+from fspath._which import *
+
+def test_import_all():
+    # import *all* names from origin modules (see imports above on module level)
+    pass
+
